b50e5b3a45cbd2e52376ef0dde31e7035325b712
Abeeha-Z <158211569+Abeeha-Z@users.noreply.github.com>
2024-10-02 12:52:59 -0400

    gtyujgt

---
+++ DOMnotes.js
@@ -0,0 +1,29 @@
+const DOMSelectors = {
+    header: document.querySelector("h1"),
+    items: document.querySelectorAll("li"),
+    form: document.querySelector(".form"),
+    //things being selected from the html file will be stores here
+}
+
+//Objects are dictionaries for js
+
+let student = {
+    name: "Ryan",
+    age:15,
+    girlfriend:true
+};
+//dictionary for instances of student
+console.long(DOMSelectors.items)
+
+DomSelectors/GamepadButton.addEventListener("click", function(event) {
+    console.long(event.target.parentElement)
+    EventSource.target.style.backgroundColor = "red";
+    //OR
+    EventSource.target.parentElement.style.backgroundColor = "red";
+
+});
+
+DOMSelectors.form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log();
+});
--- index.js
+++ index.js
@@ -1,29 +1,8 @@
 const DOMSelectors = {
-    header: document.querySelector("h1"),
-    items: document.querySelectorAll("li"),
-    form: document.querySelector(".form"),
-    //things being selected from the html file will be stores here
+    input: document.querySelector("#input")
 }
 
-//Objects are dictionaries for js
-
-let student = {
-    name: "Ryan",
-    age:15,
-    girlfriend:true
-};
-//dictionary for instances of student
-console.long(DOMSelectors.items)
-
-DomSelectors/GamepadButton.addEventListener("click", function(event) {
-    console.long(event.target.parentElement)
-    EventSource.target.style.backgroundColor = "red";
-    //OR
-    EventSource.target.parentElement.style.backgroundColor = "red";
-
-});
-
-DOMSelectors.form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    console.log();
+DOMSelectors.form.addEventListener("submit", function() {
+    let input = DOMSelectors.input.value;
+    console.log(input);
 });
