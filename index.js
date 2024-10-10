5147b3ca6f2a59720f8dc02cf4ff3a40eea1ac39
Abeeha-Z <158211569+Abeeha-Z@users.noreply.github.com>
2024-10-10 13:56:32 -0400

    jnh

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ DOMSelectors.form.addEventListener("submit", function() {
     let input = DOMSelectors.input.value;
     console.log(input);
 });
+//push input to html, and create an object(card), injects object into DOM
+
+//create a button to clear input fields and delete object
