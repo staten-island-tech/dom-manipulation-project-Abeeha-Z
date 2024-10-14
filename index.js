69978f8f68b393c6542b19f0c1c92869af433cd6
Abeeha-Z <abeehaz@nycstudents.net>
2024-10-13 21:35:03 -0400

    dfvdv

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const DOMSelectors = {
   Inputactor: document.querySelector("#movieactor"),
   Inputimage: document.querySelector("#coverimg"),
   form: document.querySelector(".form"),
-  box: document.querySelector("#container"),
-  submitButton: document.querySelector("#submitBtn"),
+  box: document.querySelector("#container"), //use for style
+  submitButton: document.querySelector("#submitBtn"), //use for style
   Output: document.querySelector(".outputcard"),
 };
 //pushed input to html
@@ -25,6 +25,7 @@ function injectObject(newCard) {
   Card.innerHTML = `
   <span>${newCard.name}, Starring: ${newCard.actor}, ${newCard.image} </span>
   `;
+  <span>${}</span>;
   DOMSelectors.Output.appendChild(Card);
 }
 
@@ -44,8 +45,4 @@ DOMSelectors.form.addEventListener("submit", function (event) {
   createObject();
 });
 
-// DOMSelectors.form.addEventListener("submit", function() {
-
-//push input to html, and create an object(card), injects object into DOM
-
-//create a button to clear input fields and delete object
+//create a reset button in the card to activate  removeObject
