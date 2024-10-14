74c4743258e07077a23ff695aa3343f5fa544cf9
Abeeha-Z <abeehaz@nycstudents.net>
2024-10-13 21:27:06 -0400

    jhgjh

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
 const DOMSelectors = {
-    input: document.querySelector(".input")
-    submit_button = document.querySelector("#submit_btn")
-    box = document.querySelector("#container")
+  Inputname: document.querySelector("#moviename"),
+  Inputactor: document.querySelector("#movieactor"),
+  Inputimage: document.querySelector("#coverimg"),
+  form: document.querySelector(".form"),
+  box: document.querySelector("#container"),
+  submitButton: document.querySelector("#submitBtn"),
+  Output: document.querySelector(".outputcard"),
+};
+//pushed input to html
+function createObject() {
+  const newCard = {
+    name: DOMSelectors.Inputname.value,
+    actor: DOMSelectors.Inputactor.value,
+    image: DOMSelectors.Inputimage.value,
+  };
+  injectObject(newCard);
+  clearInputs();
 }
 
+function injectObject(newCard) {
+  const Card = document.createElement("div");
+  Card.classList.add("moviecard");
 
+  Card.innerHTML = `
+  <span>${newCard.name}, Starring: ${newCard.actor}, ${newCard.image} </span>
+  `;
+  DOMSelectors.Output.appendChild(Card);
+}
+
+function clearInputs() {
+  DOMSelectors.Inputname.value = "";
+  DOMSelectors.Inputactor.value = "";
+  DOMSelectors.Inputimage.value = "";
+}
+
+function removeObject(Card) {
+  DOMSelectors.Output.removeChild(Card);
+}
+//Event listener to create movie card
+
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  createObject();
+});
 
 // DOMSelectors.form.addEventListener("submit", function() {
-//     let input = DOMSelectors.input.value;
-//     console.log(input);
-// });
 
-DOMSelectors.form.addEventListener("submit", function() {
-    background(DomSelectors.box)
-});
 //push input to html, and create an object(card), injects object into DOM
 
 //create a button to clear input fields and delete object
